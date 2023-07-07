698dc6f78d8dd0d5f068a858b6f27bb636a6416d
AmauryGuillin <amauryg@hotmail.fr>
2023-07-08 00:05:51 +0200

    adding CORS auth

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db_1 = __importDefault(require("./db"));
 const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept");
     next();
--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import routes from "./routes";
 const app = express();
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
